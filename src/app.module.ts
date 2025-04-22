057668876c48733ea121a8f79583d0558f5de013
leochen <leochen@acube.com.tw>
2025-04-22 15:35:40 +0800

    feat: remove static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
-import { join } from 'path';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'data/db.sqlite',
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
     AuthModule,
   ],
 })
